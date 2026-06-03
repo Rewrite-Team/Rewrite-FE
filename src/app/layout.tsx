c01691aca0a73f67570545bf687a940a7872e45e
Leeseojeong <sjung0314@gmail.com>
2026-06-03 20:32:25 +0900

    🔧 Chore: 폰트 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,12 @@
+import localFont from 'next/font/local';
+
 import '@/shared/styles/globals.css';
+
+const pretendard = localFont({
+  src: '../../public/fonts/PretendardVariable.woff2',
+  variable: '--font-pretendard',
+  display: 'swap',
+});
 
 export default function RootLayout({
   children,
@@ -6,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
+    <html className={`${pretendard.variable} ${pretendard.className}`} lang="ko">
       <body>{children}</body>
     </html>
   );
