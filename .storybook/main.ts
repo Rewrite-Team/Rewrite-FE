69ec9301c9e3a5c9d92b2cbe7ac72f92a138db80
Leeseojeong <sjung0314@gmail.com>
2026-06-12 14:40:31 +0900

    🐛 Fix: svgr 설정 스토리북에 추가

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,21 +1,60 @@
-import path from 'node:path';
-
 import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
+    '@storybook/addon-onboarding',
     '@chromatic-com/storybook',
     '@storybook/addon-a11y',
     '@storybook/addon-docs',
-    '@storybook/addon-onboarding',
   ],
   framework: {
     name: '@storybook/nextjs',
-    options: {
-      nextConfigPath: path.resolve(process.cwd(), 'next.config.ts'),
-    },
+    options: {},
   },
   staticDirs: ['../public'],
+  webpackFinal: async (config) => {
+    if (!config.module || !config.module.rules) {
+      return config;
+    }
+
+    config.module.rules = [
+      ...config.module.rules.map((rule) => {
+        if (!rule || rule === '...') {
+          return rule;
+        }
+
+        if (rule.test && /svg/.test(String(rule.test))) {
+          return { ...rule, exclude: /\.svg$/i };
+        }
+
+        return rule;
+      }),
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              svgoConfig: {
+                plugins: [
+                  {
+                    name: 'preset-default',
+                    params: {
+                      overrides: {
+                        removeViewBox: false,
+                      },
+                    },
+                  },
+                ],
+              },
+            },
+          },
+        ],
+      },
+    ];
+
+    return config;
+  },
 };
 export default config;
