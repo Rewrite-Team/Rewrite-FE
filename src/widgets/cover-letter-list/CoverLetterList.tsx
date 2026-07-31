8d3c7137948b0906f4f6d3a377eba3529bebd38e
Leeseojeong <sjung0314@gmail.com>
2026-08-01 00:18:59 +0900

    🐛 Fix: 버튼 텍스트 create로 변경

--- src/widgets/cover-letter-list/CoverLetterList.tsx
+++ src/widgets/cover-letter-list/CoverLetterList.tsx
@@ -33,7 +33,7 @@ export function CoverLetterList({ requestedPage }: CoverLetterListProps) {
           href={ROUTES.WRITING_CREATE}
           variant="outline"
         >
-          upload
+          CREATE
         </LinkButton>
       </div>
 
