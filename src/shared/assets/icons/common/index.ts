0d8dc8138c9eceac0da819ec701cc5e8cc5f8385
Leeseojeong <sjung0314@gmail.com>
2026-08-03 02:51:23 +0900

    🔧 Chore: check icon 추가

--- src/shared/assets/icons/common/index.ts
+++ src/shared/assets/icons/common/index.ts
@@ -2,6 +2,7 @@ import AltArrowDownIcon from './ic-alt-arrow-down.svg';
 import AltArrowRightIcon from './ic-alt-arrow-right.svg';
 import BackIcon from './ic-back.svg';
 import CancelIcon from './ic-cancel.svg';
+import CheckIcon from './ic-check.svg';
 import ChevronLeftIcon from './ic-chevron-left.svg';
 import ChevronRightIcon from './ic-chevron-right.svg';
 import GithubIcon from './ic-github.svg';
@@ -12,6 +13,7 @@ export {
   AltArrowRightIcon,
   BackIcon,
   CancelIcon,
+  CheckIcon,
   ChevronLeftIcon,
   ChevronRightIcon,
   GithubIcon,
