ae93d2cc1fbc509e3537244a35f68226d37d7e3a
Leeseojeong <sjung0314@gmail.com>
2026-08-03 23:16:32 +0900

    🔥 Remove: 자기소개서 임시 진행 상태 제거

--- src/features/cover-letter/create-flow/index.ts
+++
@@ -1,6 +0,0 @@
-export {
-  COVER_LETTER_CREATE_STEP_FLOW,
-  FINAL_COVER_LETTER_CREATE_STEP,
-} from './model/constants/coverLetterCreateStepFlow';
-export { CoverLetterCreateFlowProvider } from './model/providers/CoverLetterCreateFlowProvider';
-export { useCoverLetterCreateFlow } from './model/hooks/useCoverLetterCreateFlow';
--- src/features/cover-letter/create-flow/model/CoverLetterCreateFlowContext.ts
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { createContext } from 'react';
-
-import type { CoverLetterCreateFlowContextValue } from './types/CoverLetterCreateFlow.types';
-
-export const CoverLetterCreateFlowContext = createContext<CoverLetterCreateFlowContextValue | null>(
-  null
-);
