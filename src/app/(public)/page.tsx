dfb1bea8de90415a39f117f75345c18cbb32a546
Leeseojeong <sjung0314@gmail.com>
2026-09-15 01:55:20 +0900

    ✨ Feat: 랜딩 페이지 섹션 구성

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,5 +1,17 @@
+import { LandingClosing } from '@/widgets/landing/closing';
+import { LandingFeatures } from '@/widgets/landing/features';
 import { LandingHero } from '@/widgets/landing/hero';
+import { LandingPreparation } from '@/widgets/landing/preparation';
+import { LandingProcess } from '@/widgets/landing/process';
 
 export default function PublicHomePage() {
-  return <LandingHero />;
+  return (
+    <>
+      <LandingHero />
+      <LandingPreparation />
+      <LandingProcess />
+      <LandingClosing />
+      <LandingFeatures />
+    </>
+  );
 }
